5cf59082eaee7b03d4bcf4548ef0bbd64dca2135
Francesco <francescogiommi97@icloud.com>
2025-08-05 17:20:59 +0200

    Add HomePage, DetailsPage, and React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,18 @@
 import "./App.css";
-import { supabase } from "../supabase/supabaseClient";
-import type { TravelPost } from "../types";
-import { useEffect, useState } from "react";
-import { isTravelPost } from "../typeGuard/typeGuard";
+
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
 function App() {
-  const [posts, setPosts] = useState<TravelPost[]>([]);
-
-  async function fetchPosts(): Promise<TravelPost[] | null> {
-    try {
-      const response = await fetch(
-        "https://pdorueopvdnmydujmqzg.supabase.co/rest/v1/japan_travel_posts",
-        {
-          headers: {
-            apikey: import.meta.env.VITE_SUPABASE_ANON_KEY,
-            Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(
-          `Errore HTTP ${response.status} ${response.statusText}`
-        );
-      }
-
-      const dati: unknown = await response.json();
-
-      if (!Array.isArray(dati)) {
-        throw new Error("Formato dei dati non valido: atteso un array");
-      }
-
-      const validPosts = dati.filter(isTravelPost);
-
-      console.log(validPosts);
-      return validPosts;
-    } catch (error) {
-      console.error("Errore nella fetch:", error);
-      return null;
-    }
-  }
-
-  useEffect(() => {
-    fetchPosts().then((posts) => {
-      if (posts) {
-        setPosts(posts);
-      }
-    });
-  }, []);
-
   return (
     <>
-      <div className="container">
-        <h1>Diario di viaggio</h1>
-
-        <div className="row justify-content-center">
-          {posts.map((post) => (
-            <div
-              key={post.id}
-              className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
-            >
-              <div className="card h-100">
-                <img
-                  src={post.media_url}
-                  className="card-img-top post-img"
-                  alt={post.titolo}
-                />
-                <div className="card-body">
-                  <p className="card-text">{post.descrizione}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomePage />} path="/" />
+          <Route element={<DetailsPage />} path="/details" />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
