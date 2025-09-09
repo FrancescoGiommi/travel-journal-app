445c798cd22833296a14d557ad0f484a383aa36c
Francesco <francescogiommi97@icloud.com>
2025-09-09 17:20:00 +0200

    Add pagination

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -261,6 +261,35 @@ export default function HomePage() {
             </div>
           ))}
         </div>
+        <nav aria-label="..." className="mx-auto">
+          <ul className="pagination">
+            <li className="page-item">
+              <a href="#" className="page-link">
+                Previous
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li className="page-item active">
+              <a className="page-link" href="#" aria-current="page">
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
